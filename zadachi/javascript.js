ce27182f2c5ec088279a86b6486192d4ac7f114a
stel723 <ekaterinaoyyyy@yandex.ru>
2024-12-12 20:29:40 +0300

    задания

---
+++ zadachi/javascript.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (confirm('вы уверены, что хотите продолжить?')) {
+        alert('подтвержено')
+    } else{
+        alert('отменено')
+    }
+});
