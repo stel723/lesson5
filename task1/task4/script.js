2d9ae26da489aefc9b94cfe4dd383eebaf04d5e6
stel723 <ekaterinaoyyyy@yandex.ru>
2024-12-17 20:37:50 +0300

    задания

---
+++ task1/task4/script.js
@@ -0,0 +1,10 @@
+document.getElementById('yearButton').addEventListener('click', function(){
+    const year = Number(document.getElementById('yearInput').value);
+    const yearResult = document.getElementById('yearResult');
+
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        yearResult.textContent = 'високосный'
+    } else {
+        yearResult.textContent = 'не високосный'
+    }
+});
