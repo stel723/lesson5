591888b9d868858e39d976912599a8c26f30d85b
stel723 <ekaterinaoyyyy@yandex.ru>
2024-12-17 20:46:15 +0300

    задания

---
+++ task1/task5/script.js
@@ -0,0 +1,7 @@
+document.getElementById('chisloButton').addEventListener('click', function(){
+    const chislo = Number(document.getElementById('chisloInput').value);
+    const chisloResult = document.getElementById('chisloResult');
+
+    const isPalindrome = chislo == chislo.split('').reverse().join('');
+    chisloResult.textContent = isPalindrome ? 'число является палиндромом': 'число не является палиндромом';
+});
