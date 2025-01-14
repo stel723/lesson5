497801f9fc878556ec2099591f23768b7e436e97
stel723 <ekaterinaoyyyy@yandex.ru>
2025-01-14 19:47:32 +0300

    задания

---
+++ dz4/task2.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+
+function factorial(num){
+    if (num > 0){
+        return
+    }
+    if (num == 0){
+        return 1;
+    }
+    return num * factorial(num - 1);
+}
+
+let num = prompt('Введите число:');
+const result = factorial(num);
+console.log(`факториал числа: ${result}`);
