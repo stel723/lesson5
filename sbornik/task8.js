aa8b8be1653f860980bb37d17fb8927930e016b2
stel723 <ekaterinaoyyyy@yandex.ru>
2025-01-16 20:30:59 +0300

    задачи

---
+++ sbornik/task8.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+let num1 = prompt("Введите первое число:");
+let num2 = prompt("Введите второе число:");
+let num3 = prompt("Введите третье число:");
+let num4 = prompt("Введите четвёртое число:");
+console.log(`${num1} ${num2} ${num3} ${num4}`)
