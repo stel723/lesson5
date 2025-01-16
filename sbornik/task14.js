5d134eab2ca210cd29a6c2d284e6951165cc0e56
stel723 <ekaterinaoyyyy@yandex.ru>
2025-01-16 21:30:18 +0300

    задачи

---
+++ sbornik/task14.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+let a = prompt('введите a:');
+let n = prompt('введите b:');
+let x = prompt('введите x:');
+let y = prompt('введите y:');
+
+console.log(`a) 2x = ${2 * x}`);
+console.log(`б) sin x = ${Math.sin(x * ) * x}`);
