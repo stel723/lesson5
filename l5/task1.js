6fb7073ce1ec3dd05e67320fc6da588142454b94
stel723 <ekaterinaoyyyy@yandex.ru>
2025-01-21 19:10:00 +0300

    задачи

---
+++ l5/task1.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')();
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    autput: process.stdout
+});
+
+function stringFrom(...args) {
+    return args.join(" ");
+}
+
+function colectStrings(){
+    const strings = {};
+
+    function askForInput(){
+        rl.question("Введите строку или стоп:", (input) =>{
+            if (input.toLowCase() == "stop") {
+                console.log(stringFrom(...strings));
+            } else {
+                strings.push(input);
+                askForInput();
+            }
+        });
+    }
+    askForInput();
+}
+
+colectStrings();
