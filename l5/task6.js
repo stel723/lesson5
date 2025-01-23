5f1a3901a205c9bf6f8fe99516b257d452d26216
stel723 <ekaterinaoyyyy@yandex.ru>
2025-01-23 20:31:57 +0300

    задачи

---
+++ l5/task6.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')();
+
+function let(n) {
+    if(n<10) {
+        return n.toString();
+    } else {
+        return (n%10).toString() + left(Math.floor(n/10));
+    }
+}
+
+let chislo = prompt('Введите число:');
+const result = left(chislo);
+console.log(result)
