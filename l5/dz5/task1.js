42359b925b6c4b284c13ccccfe5ef308d0ebdff9
stel723 <ekaterinaoyyyy@yandex.ru>
2025-01-23 21:07:39 +0300

    задачи

--- l5/dz5/task1.js
+++ l5/dz5/task1.js
@@ -1,17 +1,17 @@
 const prompt = require('prompt-sync')();
 
-function stepen(base,exp) {
+function stepen(base, exp) {
     if (exp == 0) {
         return 1;
     }
     if (exp < 0) {
         return 1 / stepen(base, -exp);
     }
-    return base * stepen(base,exp, -1);
+    return base * stepen(base, exp -1);
 }
 
-const base = prompt('Введите число:');
-const exp = promt('Введите степень:');
-const exp = stepen(base, -exp);
+const base = prompt("Введите число: ");
+const exp = prompt("Введите степень: ");
+const result = stepen(base, exp);
 
 console.log(result);
