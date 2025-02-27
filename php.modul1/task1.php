f86be973aefb6055fbf0ad9590ac52f0b702e538
stel723 <ekaterinaoyyyy@yandex.ru>
2025-02-27 21:06:29 +0300

    модуль 1

--- php.modul1/task1.php
+++ php.modul1/task1.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Форма ввода имени</title>
+    <title>Форма ввода имени и возраста</title>
 </head>
 <body>
 
 <form method="post">
     <label for="name">Введите ваше имя:</label>
     <input type="text" id="name" name="name" required>
+
+    <label for="age">Введите ваш возраст:</label>
+    <input type="text" id="age" name="age" required>
+
     <input type="submit" value="Отправить">
 </form>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
+    $age = htmlspecialchars($_POST['age']);
 
     echo "Hello! My name is '" . $name . "'";
+    echo "I am '". $age . "'";
 }
 ?>
 
