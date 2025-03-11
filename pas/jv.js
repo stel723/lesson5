f5274a90f5af8aa71b7b014b86bbba1fa3e3c2ee
stel723 <ekaterinaoyyyy@yandex.ru>
2025-03-11 21:08:46 +0300

    something

--- pas/jv.js
+++ pas/jv.js
@@ -3,6 +3,7 @@ const suits = ['♠', '♣', '♥', '♦'];
         let deck = [];
         let gamePiles = [];
 
+        // Создание колоды
         function createDeck() {
             for (let suit of suits) {
                 for (let rank of ranks) {
@@ -12,6 +13,7 @@ const suits = ['♠', '♣', '♥', '♦'];
             shuffle(deck);
         }
 
+        // Перемешивание колоды
         function shuffle(array) {
             for (let i = array.length - 1; i > 0; i--) {
                 const j = Math.floor(Math.random() * (i + 1));
@@ -19,19 +21,22 @@ const suits = ['♠', '♣', '♥', '♦'];
             }
         }
 
+        // Рисование карт на экране
         function drawCards() {
             for (let i = 0; i < 7; i++) {
                 const pile = document.createElement('div');
                 pile.className = 'pile';
                 gamePiles.push([]);
-                
+
                 for (let j = 0; j < i + 1; j++) {
+                    const cardValue = deck.pop();
                     const card = document.createElement('div');
                     card.className = 'card';
-                    card.textContent = deck.pop();
+                    card.textContent = cardValue; // Можно заменить на изображение
+                    card.style.backgroundImage = `url('https://deckofcardsapi.com/static/img/${cardValue}.png')`;
                     card.style.top = `${30 * j}px`;
                     pile.appendChild(card);
-                    gamePiles[i].push(card.textContent);
+                    gamePiles[i].push(cardValue);
                 }
                 document.getElementById('game').appendChild(pile);
             }
