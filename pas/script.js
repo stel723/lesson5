f8860270979d8d74c20425ea834a4f6a57342631
stel723 <ekaterinaoyyyy@yandex.ru>
2025-04-03 20:41:12 +0300

    пасьянс

--- pas/script.js
+++ pas/script.js
@@ -1,5 +1,5 @@
 const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
-const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const ranks = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 let deck = [];
 let score = 0;
 let tableau = [];
